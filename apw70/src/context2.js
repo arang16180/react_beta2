d639aa7ed9692f6dcf84fe5537697b8acb1cae1a
Angel Rangel <105447969+arang16180@users.noreply.github.com>
2023-09-28 11:23:20 -0600

    Delete apw70/src directory

--- apw70/src/context2.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-
-export const SuperContext01 = createContext({users:[{name:'Guest',email:'-',password:'-',balance:0,id:0},{name:'Albert Einstein',email:'albert_einstein@mit.edu',password:'secret123',balance:160,id:1}]});
-export const GlobalId01 = createContext({number:0});
